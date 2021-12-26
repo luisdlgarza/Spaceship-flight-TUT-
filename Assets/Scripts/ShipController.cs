dd3bb2025bfa94c9eb2415fb7d69fdf8066664d2
luisdlgarza <luisdlgarza2@gmail.com>
2021-12-26 13:49:26 -0500

    qd

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    public float forwardSpeed = 25f , strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
+        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
+        activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
+
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+
+    }
+}
