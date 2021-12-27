1cdd7078f3d4de094351f8a8170e76dd547c45ad
luisdlgarza <luisdlgarza2@gmail.com>
2021-12-27 01:18:10 -0500

    shipping

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -12,29 +12,46 @@ public class ShipController : MonoBehaviour
     public float lookRateSpeed = 90f;
     private Vector2 lookInput, screenCenter, mouseDistance;
 
+    private float rollinput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         screenCenter.x = Screen.width * .5f;
         screenCenter.y = Screen.height * .5f;
+
+        activeForwardSpeed = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
+
         lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
 
-        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.x;
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
         mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
 
-        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime , 0f, Space.Self );
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollinput = Mathf.Lerp(rollinput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime , rollinput * rollSpeed * Time.deltaTime, Space.Self );
 
 
         activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
         activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
         activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        if (activeForwardSpeed < 10f)
+        {
+            activeForwardSpeed = 10f;
+        }
+
 
         transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
         transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
